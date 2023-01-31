d99d7a841ff19a206318c019a3b08a333d4dc5fe
AlessandroLembo <alessandrolembo2@gmail.com>
2023-01-31 21:00:57 +0100

    section jumbo blocks

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -16,11 +16,37 @@ export default {
 </script>
 
 <template>
+    <!-- to jumbo slider -->
     <jumbo-slider></jumbo-slider>
+    <section id="jumbo-udemy">
+        <h1>titolo</h1>
+        <figure>
+            <!-- <img src="../assets/images/slide-1.jpg" alt=""> -->
+            <p>img</p>
+        </figure>
+    </section>
     <course-card v-for="course in courses" :key="course.id" :course="course"></course-card>
 
 </template>
 
-<style>
+<style scoped lang="scss">
+@use '../assets/scss/partials/variables' as *;
+@use '../assets/scss/partials/verAlignm' as *;
 
+#jumbo-udemy {
+    background-color: #ff4450;
+    height: 300px;
+    @include vertical-flex;
+    justify-content: space-between;
+
+    figure {
+        // width: 100%;
+        height: 100%;
+
+        img {
+            height: $h100;
+            object-fit: cover;
+        }
+    }
+}
 </style>
