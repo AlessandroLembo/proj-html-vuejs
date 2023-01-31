1038b6beca0b8e7e29db8334fd4b579bd53bd064
AlessandroLembo <alessandrolembo2@gmail.com>
2023-01-31 16:52:27 +0100

    created slider component

--- src/data/index.js
+++ src/data/index.js
@@ -147,18 +147,4 @@ const courses = [
 
 ]
 
-
-
-const api = {
-    language: 'it-IT',
-    key: 'dd9b1073e48ec92b6d25fb7c351682de',
-    baseUri: 'https://api.themoviedb.org/3/'
-    
-    }
-
-const pics = {
-    baseUrl: 'https://image.tmdb.org/t/p/w342',
-    placeholder: 'https://www.altavod.com/assets/images/poster-placeholder.png'
-}
-    
     export { courses };
