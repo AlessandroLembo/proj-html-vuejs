0ded195cfb76bbeb5a3fe2565077d708dd50bd26
AlessandroLembo <alessandrolembo2@gmail.com>
2023-01-31 20:30:44 +0100

    imported jumbo slider in App main

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,7 @@ export default {
     <app-main></app-main>
     <app-footer></app-footer>
 
+
 </template>
 
 <style lang="scss">
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,10 @@
 <script>
 import { courses } from '../data';
 import CourseCard from './slider/CourseCard.vue';
+import JumboSlider from './slider/JumboSlider.vue';
 export default {
     name: 'App Main',
-    components: { CourseCard },
+    components: { CourseCard, JumboSlider },
     data() {
         return {
             courses
@@ -15,7 +16,9 @@ export default {
 </script>
 
 <template>
+    <jumbo-slider></jumbo-slider>
     <course-card v-for="course in courses" :key="course.id" :course="course"></course-card>
+
 </template>
 
 <style>
