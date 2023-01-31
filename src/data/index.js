0a80212eacbee7443e238eb70cba0eae18df3f9c
AlessandroLembo <alessandrolembo2@gmail.com>
2023-01-31 14:47:29 +0100

    imported images and start to create courses' array

---
+++ src/data/index.js
@@ -0,0 +1,28 @@
+const courses = [
+    {
+       id: 1, 
+       categore: 'Development >',
+       name: 'The Complete iOS 10 & Swift 3 Developer Course',
+       vote_average: 4,
+       price: '$100',
+       original_cost: '$199.99',
+
+
+    }
+]
+
+
+
+const api = {
+    language: 'it-IT',
+    key: 'dd9b1073e48ec92b6d25fb7c351682de',
+    baseUri: 'https://api.themoviedb.org/3/'
+    
+    }
+
+const pics = {
+    baseUrl: 'https://image.tmdb.org/t/p/w342',
+    placeholder: 'https://www.altavod.com/assets/images/poster-placeholder.png'
+}
+    
+    export { courses };
